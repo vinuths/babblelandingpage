90323f2e0de52b50ec34be8368a82a4a14474e55
ArjunDev10 <arjun@matrixhrservices.com>
2024-12-12 13:31:37 +0530

    aud basename check

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ import 'react-tabs/style/react-tabs.css';
 
 function App() {
   return (
-    <Router>
+    <Router  basename="/auditor">
       <Navbar />
           <Routes>
               <Route exact path="/" element={<Login />} />
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-import {  Typography,FormGroup,FormControl,TextField,styled,Button, FormLabel,Avatar,InputAdornment,IconButton } from '@mui/material';
+import { Typography, FormGroup, FormControl, TextField, styled, Button, Avatar, InputAdornment, IconButton, Grid, Box } from '@mui/material';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import EmailIcon from '@mui/icons-material/Email';
@@ -12,6 +12,8 @@ import { toast } from 'react-toastify';
 import * as Yup from 'yup'; // Yup is a JavaScript object schema validator.
 import { useFormik } from 'formik'; //formik is third party React form library. It provides basic form programming and validation
 import {useDispatch,useSelector} from 'react-redux';
+import Logo1 from '../../Logo1.png';
+
 const Login = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -62,73 +64,93 @@ const Login = () => {
     },[userInfo]);
     return (
 
-        <Container>
-            <center><Avatar style={{alignItems:'center'}} sx={{ bgcolor: deepOrange[500] }} /></center>
-            <Typography  style={{textAlign:'center'}}><Ptags>(All the field having * are required)</Ptags></Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', borderRadius:'10px'}}>
+      <Grid container spacing={2} sx={{ maxWidth: '900px' }}>
+        <Grid item xs={12} md={6}sx={{ backgroundColor: '#2a74cb', paddingBottom:'20px', borderRadius:'10px'  }}>
+          <center>
+            <h5 style={{color:'white'}}>Matrix HR Technologies Pvt. Ltd.</h5>
+            <img style={{ width: '300px', paddingTop:'30px' }} src={Logo1} alt="Logo" /><br/><br/>
+            <span style={{color:'white'}}>Stewardship in Servicing clients with <br/>Entrepreneuiral attitude adding lots of Passion and <br/> adopting Diversity to achive Excellence.</span>
+          </center>
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ backgroundColor: 'white', padding:' 16px 16px 16px', borderRadius:'10px' }}>
+            <h3 >Company Login</h3>
+          <Container>
+            <Typography style={{ textAlign: 'left',  }}>
+              <Ptags style={{  color:'#9c9390',  }}>Sign In to your account</Ptags>
+            </Typography>
             {loading && <Loading />}
-            {/* <Form metod="post" onSubmit={(e) =>{ onSubmit(e)}}> */}
-                <FormControl >
-                    <TextField value={formik.values.email} 
-                            required='required'
-                            id="email"
-                            name="email" 
-                           // label="Email"  
-                            label={<EmailIcon />} 
-                            onChange={formik.handleChange} 
-                            inputProps={{ maxLength: 50 }}
-                            error={formik.touched.email && Boolean(formik.errors.email)}
-                            helperText={formik.touched.email && formik.errors.email} />
-                </FormControl> 
-                <FormControl>
-                    <TextField value={formik.values.password} 
-                            required='required' 
-                            id='password' 
-                            name='password' 
-                            //label="Password"  
-                            label={<LockIcon />}
-                            onChange={formik.handleChange} 
-                            /* inputProps={{ maxLength: 50 }} */
-                            error={formik.touched.password && Boolean(formik.errors.password)}
-                            helperText={formik.touched.password && formik.errors.password} 
-                            type={values.showPassword ? 'text' : 'password'} 
-                            InputProps={{endAdornment: (
-                                
-                                    <InputAdornment position="end">
-                                      <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        edge="end"
-                                      >
-                                        {values.showPassword ?  <Visibility /> : <VisibilityOff />}
-                                      </IconButton>
-                                    </InputAdornment>
-                                  
-                            )}} />
-                </FormControl> 
-
-                <FormControl>
-                    <Buttons variant="contained" type="submit" id="submitting" onClick={formik.handleSubmit}>Login</Buttons>
-                </FormControl>
-        </Container>
+            <FormControl fullWidth>
+              <TextField
+                value={formik.values.email}
+                required="required"
+                id="email"
+                name="email"
+                label={<EmailIcon />}
+                onChange={formik.handleChange}
+                inputProps={{ maxLength: 50 }}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <TextField
+                value={formik.values.password}
+                required="required"
+                id="password"
+                name="password"
+                label={<LockIcon />}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+                type={values.showPassword ? 'text' : 'password'}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                      >
+                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+            <br/>
+            <FormControl fullWidth>
+              <Buttons variant="contained" type="submit" id="submitting" onClick={formik.handleSubmit}>
+                Login
+              </Buttons>
+            </FormControl>
+          </Container>
+        </Grid>
+      </Grid>
+    </Box>
 
     )
 }
 
 export default Login;
 const Container = styled(FormGroup)`
-width: 30%;
-margin: 8% auto 0 auto;
-& > div {
-    margin-top:10px;
-}
-`
+  width: 100%;
+  margin-top: 20px;
+  & > div {
+    margin-top: 10px;
+  }
+`;
+
 const Buttons = styled(Button)`
-width: 100%;
-line-height: 3.0;
-`
+  width: 100%;
+  line-height: 3.0;
+  background-color: #2a74cb;
+`;
+
 const Ptags = styled('p')`
-font-weight: 500;
-letter-spacing: -0.025em;
-color: #253992;
-line-height: 1.2;
-`
+  font-weight: 500;
+  letter-spacing: -0.025em;
+  color: #253992;
+  line-height: 1.2;
+`;
